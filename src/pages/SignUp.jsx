bd8cffc5ad384dbf4319acd9ac87d4a7e5818a54
sohee <sohee5143@naver.com>
2024-04-13 02:30:58 +0900

    Feat: 인증번호 입력 창 추가

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -14,7 +14,27 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Button from "../components/common/Button";
 
 export default function SignUp() {
-  const [passwordError, setPasswordError] = useState(false); // 에러 상태
+  const [passwordError, setPasswordError] = useState(false); // 비밀번호 유효성 에러 상태
+  const [showVerificationInput, setShowVerificationInput] = useState(false); // 인증번호 창 뜨기/안뜨기 상태
+  const [confirmVerificationCode, setConfirmVerificationCode] = useState(false); // 인증번호 확인 완료 상태
+  const [currentStep, setCurrentStep] = useState(1); // 현재 회원가입 단계 상태
+  // 인증번호 발송 버튼
+  const handleSendButtonClick = () => {
+    setShowVerificationInput(true);
+  };
+  // 인증번호 확인 버튼
+  const handleConfirmVerificationCode = () => {
+    setConfirmVerificationCode(true);
+  };
+  // 단계가 변경될 때마다 단계 색 바꿈
+  const getCircleColor = (step) => {
+    return currentStep === step ? "#829FD7" : "#ECECEE";
+  };
+  // 다음 버튼 클릭
+  const handleNextStep = () => {
+    setCurrentStep(currentStep + 1);
+  };
+
   //폰트 설정
   const theme = createTheme({
     typography: {
@@ -57,107 +77,163 @@ export default function SignUp() {
         <SubText>레코드 타임라인의 회원이 되어 함께 성장해요!</SubText>
         <NumBox>
           <StageWrap>
-            <NumCircle marginLeft="20px">1</NumCircle>
+            <NumCircle
+              marginLeft="20px"
+              style={{ backgroundColor: getCircleColor(1) }}
+            >
+              1
+            </NumCircle>
             <NumLine />
-            <NumCircle marginRight="20px">2</NumCircle>
+            <NumCircle
+              marginRight="20px"
+              style={{ backgroundColor: getCircleColor(2) }}
+            >
+              2
+            </NumCircle>
           </StageWrap>
+
           <NumText>이메일/비밀번호 입력</NumText>
         </NumBox>
-        <ThemeProvider theme={theme}>
-          <Container
-            component="main"
-            sx={{
-              margin: "0px 16px 0px 0px",
-              maxWidth: "560px",
-              width: "560px",
-            }}
-          >
-            <CssBaseline />
-            <FormControl
-              component="fieldset"
-              variant="standard"
-              sx={{
-                marginTop: 8,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <Grid container spacing={2}>
-                <EmailBox>
-                  <Grid
-                    item
-                    xs={12}
-                    sm={9}
-                    sx={{
-                      padding: "16px 0px 0px 13px",
-                    }}
-                  >
-                    <StyledTextField
-                      required
-                      autoFocus
-                      fullWidth
-                      type="email"
-                      id="email"
-                      name="email"
-                      label="이메일"
-                      InputProps={{
-                        style: {
-                          borderRadius: "15px",
-                          width: "380px",
-                        },
-                      }}
-                    />
+        {currentStep === 1 && (
+          <>
+            <ThemeProvider theme={theme}>
+              <Container
+                component="main"
+                sx={{
+                  margin: "0px 16px 0px 0px",
+                  maxWidth: "560px",
+                  width: "560px",
+                }}
+              >
+                <CssBaseline />
+                <FormControl
+                  component="fieldset"
+                  variant="standard"
+                  sx={{
+                    marginTop: 8,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <Grid container spacing={2}>
+                    <EmailBox>
+                      <Grid
+                        item
+                        xs={12}
+                        sm={9}
+                        sx={{
+                          padding: "16px 0px 0px 13px",
+                        }}
+                      >
+                        <StyledTextField
+                          required
+                          autoFocus
+                          fullWidth
+                          type="email"
+                          id="email"
+                          name="email"
+                          label="이메일"
+                          InputProps={{
+                            style: {
+                              borderRadius: "15px",
+                              width: "380px",
+                            },
+                          }}
+                        />
+                      </Grid>
+                      <ConfirmButton>중복확인</ConfirmButton>
+                      <SendButton onClick={handleSendButtonClick}>
+                        인증번호 발송
+                      </SendButton>
+                    </EmailBox>
+                    {showVerificationInput && (
+                      <EmailBox>
+                        <Grid
+                          item
+                          xs={12}
+                          sm={9}
+                          sx={{
+                            padding: "16px 0px 0px 13px",
+                          }}
+                        >
+                          <StyledTextField
+                            required
+                            autoFocus
+                            fullWidth
+                            type="verificationCode"
+                            id="verificationCode"
+                            name="verificationCode"
+                            label="인증번호 입력"
+                            InputProps={{
+                              style: {
+                                borderRadius: "15px",
+                                width: "380px",
+                              },
+                            }}
+                          />
+                        </Grid>
+                        <SendButton onClick={handleConfirmVerificationCode}>
+                          확인
+                        </SendButton>
+                      </EmailBox>
+                    )}
+                    {confirmVerificationCode && (
+                      <ConfirmMessage>인증번호가 일치합니다</ConfirmMessage>
+                    )}
+                    <Grid item xs={12}>
+                      <StyledTextField
+                        required
+                        fullWidth
+                        type="password"
+                        id="password"
+                        name="password"
+                        label="비밀번호 (숫자+영문 8자리 이상)"
+                        InputProps={{
+                          style: {
+                            borderRadius: "15px",
+                            width: "510px",
+                          },
+                        }}
+                        onChange={handlePasswordChange}
+                        error={passwordError}
+                        helperText={
+                          passwordError
+                            ? "숫자와 영문자를 포함한 8자리 이상의 비밀번호를 입력하세요."
+                            : ""
+                        }
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <StyledTextField
+                        required
+                        fullWidth
+                        type="password"
+                        id="rePassword"
+                        name="rePassword"
+                        label="비밀번호 재입력"
+                        InputProps={{
+                          style: {
+                            borderRadius: "15px",
+                            width: "510px",
+                          },
+                        }}
+                      />
+                    </Grid>
                   </Grid>
-                  <ConfirmButton>중복확인</ConfirmButton>
-                  <SendButton>인증번호 발송</SendButton>
-                </EmailBox>
-                <Grid item xs={12}>
-                  <StyledTextField
-                    required
-                    fullWidth
-                    type="password"
-                    id="password"
-                    name="password"
-                    label="비밀번호 (숫자+영문 8자리 이상)"
-                    InputProps={{
-                      style: {
-                        borderRadius: "15px",
-                        width: "510px",
-                      },
-                    }}
-                    onChange={handlePasswordChange}
-                    error={passwordError}
-                    helperText={
-                      passwordError
-                        ? "숫자와 영문자를 포함한 8자리 이상의 비밀번호를 입력하세요."
-                        : ""
-                    }
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <StyledTextField
-                    required
-                    fullWidth
-                    type="password"
-                    id="rePassword"
-                    name="rePassword"
-                    label="비밀번호 재입력"
-                    InputProps={{
-                      style: {
-                        borderRadius: "15px",
-                        width: "510px",
-                      },
-                    }}
-                  />
-                </Grid>
-              </Grid>
-              <Button width="170px" height="50px" margin="40px 0px 0px 0px">
-                다음
-              </Button>
-            </FormControl>
-          </Container>
-        </ThemeProvider>
+                  <Button
+                    width="170px"
+                    height="50px"
+                    margin="40px 0px 0px 0px"
+                    onClick={handleNextStep}
+                  >
+                    다음
+                  </Button>
+                </FormControl>
+              </Container>
+            </ThemeProvider>
+          </>
+        )}
       </SignUpBox>
     </SignUpWrap>
   );
@@ -265,4 +341,10 @@ const EmailBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const ConfirmMessage = styled.div`
+  font-size: 15px;
+  color: #0a8425;
+  margin: 5px 0px 0px 20px;
 `;

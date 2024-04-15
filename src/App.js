618844840b9a4ed1a01137bb29faaf2024fcb8ee
sohee <sohee5143@naver.com>
2024-04-16 00:36:44 +0900

    Feat: 사이드 바 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Login from "./pages/Login";
-
+import SideBar from "./components/common/SideBar";
 function App() {
   return (
     <div className="App">
+      <SideBar />
+
       <Routes>
         <Route path="/" element={<Main />} /> {/* 메인 */}
         <Route path="/login" element={<Login />} /> {/* 로그인 페이지 */}

54bb6ea83975cffeff14508e520049d04044034a
sohee <sohee5143@naver.com>
2024-04-27 23:29:37 +0900

    Feat: 네비게이션 바 프로필 추가

--- src/components/common/NavigationBar.jsx
+++ src/components/common/NavigationBar.jsx
@@ -4,10 +4,14 @@ import Logo from "../../assets/images/recodeTimelineLogo.svg";
 import { css } from "@emotion/react";
 import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
 import MenuIcon from "@mui/icons-material/Menu";
+import CreateIcon from "@mui/icons-material/Create";
 import { useState } from "react";
+import testProfileImg from "../../assets/images/testProfileImg.png";
 export default function NavigationBar() {
   const [isExpanded, setIsExpanded] = useState(true); // 네비게이션 바 펼친 상태 & 접힌 상태
-
+  const nickName = "닉네임"; // 테스트 닉네임
+  const interestCategory = "개발자"; // 테스트 관심분야
+  const introduction = "어쩌구 저쩌구 하고싶은 웹 디벨로퍼 누구누구 입니다~"; // 테스트 소개글
   const toggleNavigationBar = () => {
     setIsExpanded(!isExpanded);
   };
@@ -19,7 +23,7 @@ export default function NavigationBar() {
         height: 100vh;
         display: flex;
         flex-direction: column;
-        transition: width 0.2s ease; // 네비게이션 바 없어지는 모션
+        transition: width 0.4s ease; // 네비게이션 바 생기는 모션
       `}
     >
       <div
@@ -29,24 +33,102 @@ export default function NavigationBar() {
           margin: "30px 20px 0px 38px",
         })}
       >
-        <img
-          src={Logo}
-          alt="record timeline"
-          css={css`
-            width: ${isExpanded ? "180px" : "0px"};
-            overflow: hidden;
-          `}
-        />
         {isExpanded ? (
-          <KeyboardDoubleArrowLeftIcon
-            onClick={toggleNavigationBar}
-            style={{
-              fontSize: "28px",
-              color: "#666666",
-              cursor: "pointer",
-              marginLeft: "auto",
-            }}
-          />
+          <div>
+            <div
+              css={css({
+                display: "flex",
+                alignItems: "center",
+              })}
+            >
+              <img
+                src={Logo}
+                alt="record timeline"
+                css={css`
+                  width: ${isExpanded ? "180px" : "0px"};
+                `}
+              />
+              <KeyboardDoubleArrowLeftIcon
+                onClick={toggleNavigationBar}
+                style={{
+                  fontSize: "28px",
+                  color: "#666666",
+                  cursor: "pointer",
+                  margin: "0px 0px -3px 70px",
+                }}
+              />
+            </div>
+            <div
+              css={css({
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "column",
+                margin: "40px 0px 20px 0px",
+                fontSize: "22px",
+                fontWeight: "500",
+              })}
+            >
+              <img
+                src={testProfileImg}
+                alt="프로필 이미지"
+                css={css({
+                  width: "150px",
+                  marginBottom: "20px",
+                })}
+              />
+              <div>{nickName}</div>
+            </div>
+            <div
+              css={css({
+                display: "flex",
+                flexDirection: "column",
+              })}
+            >
+              <div
+                css={css({
+                  width: "95px",
+                  borderRadius: "20px",
+                  backgroundColor: "#829FD7",
+                  color: "white",
+                  textAlign: "center",
+                  fontSize: "18px",
+                  fontWeight: "200",
+                  padding: "8px 0px",
+                })}
+              >
+                {interestCategory}
+              </div>
+              <div
+                css={css({
+                  display: "flex",
+                  alignItems: "center",
+                })}
+              >
+                <textarea
+                  css={css({
+                    width: "80%",
+                    border: "none",
+                    borderLeft: "3px solid black",
+                    height: "50px",
+                    outline: "none",
+                    fontSize: "15px",
+                    padding: "5px 0px 5px 20px",
+                    marginTop: "20px",
+                    lineHeight: "140%",
+                    resize: "none",
+                  })}
+                ></textarea>
+                <CreateIcon
+                  style={{
+                    fontSize: "25px",
+                    color: "#333333",
+                    cursor: "pointer",
+                    marginTop: "20px",
+                  }}
+                />
+              </div>
+            </div>
+          </div>
         ) : (
           <>
             <MenuIcon

cc4761de83db456e8b02a505b4d5c9632d01c70d
saralove20 <saralove20@naver.com>
2024-06-07 13:05:51 +0900

    Fix: 다른 사용자 메인타임라인 간 선 연결 제거

--- src/pages/OthersMainTimeline.jsx
+++ src/pages/OthersMainTimeline.jsx
@@ -66,18 +66,18 @@ export default function OthersMainTimeline() {
         {profile && profile.nickname} 님의 Timeline
       </div>
       <div
-        css={css({ // 타임라인 간의 선
-          position: "relative", // 상대적 위치 설정
-          '&::after': { // 타임라인 아이템들 사이에 선 추가
-            content: '""',
-            position: 'absolute',
-            left: '617px', // 체크 표시 위치에 맞게 조정 --> 선 위치 절대적인거 수정해야됨
-            top: '48px', // 첫 번째 아이템의 중간부터 시작
-            width: '2px',
-            height: 'calc(100% - 75px)', // 전체 높이에서 첫 번째 아이템 높이만큼 제외
-            backgroundColor: '#E2E2E2', // 선의 색상
-          },
-        })}
+        // css={css({ // 타임라인 간의 선
+        //   position: "relative", // 상대적 위치 설정
+        //   '&::after': { // 타임라인 아이템들 사이에 선 추가
+        //     content: '""',
+        //     position: 'absolute',
+        //     left: '617px', // 체크 표시 위치에 맞게 조정 --> 선 위치 절대적인거 수정해야됨
+        //     top: '48px', // 첫 번째 아이템의 중간부터 시작
+        //     width: '2px',
+        //     height: 'calc(100% - 75px)', // 전체 높이에서 첫 번째 아이템 높이만큼 제외
+        //     backgroundColor: '#E2E2E2', // 선의 색상
+        //   },
+        // })}
       >
         {items.map((item, index) => (
           <OthersTimelineItem

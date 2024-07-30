e81c2c3d91c1abffe0a40ddb6392c9238fc37f99
sohee <sohee5143@naver.com>
2024-07-30 17:10:43 +0900

    Feat: 팔로우/팔로잉 목록 Tab 생성

--- src/pages/Follow.jsx
+++ src/pages/Follow.jsx
@@ -2,16 +2,107 @@
 
 import React from "react";
 import { css } from "@emotion/react";
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 export default function Bookmark() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div
       css={css`
-        display: flex;
-        justify-content: center;
+          display: flex;
+          justify-content: center;
+          flex-direction: column;
+          align-items: center;
       `}
     >
-      팔로우/팔로잉 페이지
+      <Box
+        sx={{
+          width: '100%',
+          marginTop: '100px',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <Box
+          sx={{
+            borderBottom: 1,
+            borderColor: 'divider',
+            width: '100%',
+          }}
+        >
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="follow/following tabs"
+            centered
+            sx={{
+              '& .MuiTab-root': {
+                fontSize: '19px',
+                minWidth: '150px',
+                fontFamily: "Pretendard"
+              },
+              '& .MuiTabs-flexContainer': {
+                justifyContent: 'center',
+              },
+            }}
+          >
+            <Tab label="팔로우" {...a11yProps(0)} />
+            <Tab label="팔로잉" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          width: '100%',
+          maxWidth: '600px',
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <CustomTabPanel value={value} index={0}>
+          팔로우 목록
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1}>
+          팔로잉 목록
+        </CustomTabPanel>
+      </Box>
     </div>
   );
 }

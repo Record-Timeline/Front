1047420b44967823f08d16d1fe69e06a40acaff9
saralove20 <saralove20@naver.com>
2024-08-12 19:35:31 +0900

    Design: 내 메인 타임라인 간 선 연결 추가

--- src/components/othersTimeline/OthersTimelineItem.jsx
+++ src/components/othersTimeline/OthersTimelineItem.jsx
@@ -44,6 +44,7 @@ export default function OthersTimelineItem({memberId, mainTimelineId, startDate,
               margin-right: 15px;
               background-color: ${isDone ? "#829FD7" : "#f8f6f6"}; // 선 때문에 뚫린 원일 때도 배경 색 설정
               position: relative; // 이 요소를 상대 위치로 설정
+              z-index: 6;
           `}
         />
         <div // 기간
@@ -85,11 +86,12 @@ export default function OthersTimelineItem({memberId, mainTimelineId, startDate,
         <div
           css={css({
             position: "absolute",
-            top: "75px", // 아이템 바로 아래에서 시작
+            top: "48px", // 아이템 바로 아래에서 시작
             left: "9%", // 체크 표시 중앙에 위치
             width: "2px",
-            height: "40px", // 다음 아이템과의 간격 맞춤
+            height: "95px", // 다음 아이템과의 간격 맞춤
             backgroundColor: "#E2E2E2",
+            zIndex: 5
           })}
         />
       )}

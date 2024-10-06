c5528d92584d88803c0bede6c77fcb26feb3622e
sohee <sohee5143@naver.com>
2024-10-06 21:16:37 +0900

    Feat: pdf 크기 변경

--- src/components/common/PdfShareButton.jsx
+++ src/components/common/PdfShareButton.jsx
@@ -10,14 +10,14 @@ export default function PdfButton({ targetId }) {
   const generatePDF = async () => {
     const timelineElement = document.querySelector("#timeline");
     const button = document.querySelector("#button");
-    button.style.display = "none";
+    button.style.display = "none"; // 버튼 숨기기
 
     // HTML 요소의 실제 크기 계산
-    const canvas = await html2canvas(timelineElement, { scale: 2 }); // 해상도 높이기 위해 scale 2 배
+    const canvas = await html2canvas(timelineElement, { scale: 3 });
 
     const imageData = canvas.toDataURL("image/png");
 
-    // PDF 크기 설정 (A4 비율, 210mm x 297mm)
+    // PDF 설정 (A4 비율)
     const pdf = new jsPDF("p", "mm", "a4");
 
     const pageWidth = pdf.internal.pageSize.getWidth();
@@ -26,12 +26,12 @@ export default function PdfButton({ targetId }) {
     // 여백 설정
     const marginTop = 10;
     const marginBottom = 10;
-    const marginLeft = 0;
-    const marginRight = 0;
+    const marginLeft = -50;
+    const marginRight = -50;
 
     // 이미지의 실제 크기 계산 (픽셀 단위에서 밀리미터 단위로 변환)
     const imgProps = pdf.getImageProperties(imageData);
-    const imgWidth = pageWidth - marginLeft - marginRight;
+    const imgWidth = pageWidth - marginLeft - marginRight; // 페이지 너비에 맞게 이미지 크기 조정
     const imgHeight = (imgProps.height * imgWidth) / imgProps.width;
 
     let heightLeft = imgHeight;
@@ -39,10 +39,9 @@ export default function PdfButton({ targetId }) {
 
     // 첫 페이지에 이미지 추가
     pdf.addImage(imageData, "PNG", marginLeft, position, imgWidth, imgHeight);
-
     heightLeft -= pageHeight - marginTop - marginBottom;
 
-    // 다음 페이지로 이미지가 넘어갈 경우 추가
+    // 페이지를 넘기면서 이미지 추가
     while (heightLeft > 0) {
       position = -heightLeft + marginTop; // 새로운 페이지의 시작 위치에서 여백만큼 띄움
       pdf.addPage();
@@ -50,8 +49,9 @@ export default function PdfButton({ targetId }) {
       heightLeft -= pageHeight - marginTop - marginBottom; // 남은 이미지 높이 업데이트
     }
 
+    // PDF 저장
     pdf.save("timeline.pdf");
-    button.style.display = "flex";
+    button.style.display = "flex"; // 버튼 다시 표시
   };
 
   return (
